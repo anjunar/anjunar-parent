4b192017a70b9c751b5fc277f2c24569ad80a0ec
Patrick Bittner <anjunar@gmx.de>
2021-06-20 22:47:33 +0200

    Date and Time Format

--- application/src/main/java/de/bitvale/anjunar/ObjectMapperProvider.java
+++ application/src/main/java/de/bitvale/anjunar/ObjectMapperProvider.java
@@ -4,14 +4,20 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import de.bitvale.anjunar.jackson.UTCLocalDateTimeDeserializer;
-import de.bitvale.anjunar.jackson.UTCLocalDateTimeSerializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.InstantSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import de.bitvale.anjunar.fasterxml.UTCInstantDeserializer;
+import de.bitvale.anjunar.fasterxml.UTCInstantSerializer;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
 import javax.ws.rs.ext.ContextResolver;
 import javax.ws.rs.ext.Provider;
+import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.TimeZone;
 
 /**
  * @author Patrick Bittner on 25.05.2015.
@@ -25,12 +31,15 @@ public class ObjectMapperProvider implements ContextResolver<ObjectMapper> {
     public ObjectMapper getContext(Class<?> type) {
         ObjectMapper objectMapper = new ObjectMapper()
                 .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
-                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                .setTimeZone(TimeZone.getDefault());
 
         JavaTimeModule module = new JavaTimeModule();
 
-        module.addSerializer(LocalDateTime.class, new UTCLocalDateTimeSerializer());
-        module.addDeserializer(LocalDateTime.class, new UTCLocalDateTimeDeserializer());
+        module.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss")));
+        module.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss")));
+        module.addSerializer(Instant.class, new UTCInstantSerializer());
+        module.addDeserializer(Instant.class, new UTCInstantDeserializer());
 
         objectMapper.registerModule(module);
 
